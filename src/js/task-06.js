943be7bdc9d807deba5e7bc107b0752943cdaafd
жора <128640675+zhorik07@users.noreply.github.com>
2023-08-16 02:49:33 +0200

    Delete src/js directory

--- src/js/task-06.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector('#validation-input');
-const length = input.dataset.length;
-input.addEventListener('blur', elem => {
-  if (elem.currentTarget.value.length === Number(length)) {
-    input.classList = 'valid';
-  } else {
-    input.classList = 'invalid';
-  }
-});
