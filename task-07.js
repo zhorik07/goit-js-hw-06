3d3f135fb829746526588659ce6e9388397ee889
жора <128640675+zhorik07@users.noreply.github.com>
2023-08-16 19:37:00 +0200

    Delete task-07.js

--- task-07.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector('#font-size-control');
-const span = document.querySelector('#text');
-input.value = '16'
-input.addEventListener('input', elem => {
-  span.style.fontSize = `${elem.currentTarget.value}px`;
-});
